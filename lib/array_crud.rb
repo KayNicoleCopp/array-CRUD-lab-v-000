1c371358e6e2b79f6930f5099b8be34f9d2ceb7c
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-09-28 20:43:43 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-["Kayla", "Nate", "Everly", "New baby"]
+ ["Kayla", "Nate", "Everly", "New baby"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+ array.unshift
 end
 
 def add_element_to_start_of_array(array, element)
